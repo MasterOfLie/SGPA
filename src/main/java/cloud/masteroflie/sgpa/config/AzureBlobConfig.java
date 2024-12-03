f13115c6d269700377f54b489ced502beda94742
Matheus Henrique <mhapessoal@gmail.com>
2024-12-03 19:23:38 -0300

    Portal Update

---
+++ src/main/java/cloud/masteroflie/sgpa/config/AzureBlobConfig.java
@@ -0,0 +1,29 @@
+package cloud.masteroflie.sgpa.config;
+
+import com.azure.storage.blob.*;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class AzureBlobConfig {
+
+    @Value("${azure.storage.connection-string}")
+    private String connectionString;
+
+    @Value("${azure.storage.container-name}")
+    private String containerName;
+
+    public BlobClient blobClient(String fileName){
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(connectionString)
+                .buildClient();
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+        if (!containerClient.exists()) {
+            containerClient.create();
+        }
+        return containerClient.getBlobClient(fileName);
+    }
+}
